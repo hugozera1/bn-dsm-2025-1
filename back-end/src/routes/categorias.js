423a0b561697b58d864cd001c884ef21dec35641
hugozera1 <hugorodrigues1052@gmail.com>
2025-03-24 21:59:45 -0300

    AULA DIA 24032025

---
+++ back-end/src/routes/categorias.js
@@ -0,0 +1,10 @@
+import { Router } from 'express' 
+import controller from '../controllers/categorias.js'
+
+
+
+const router = Router()
+
+router.post('/', controller.create)
+
+export default router 
